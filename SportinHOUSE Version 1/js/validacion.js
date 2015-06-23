178aa93918ad17743e0a9b9b086a28e1f19f5249
andresitoo <andymorale@hotmail.com>
2015-06-23 18:53:58 -0500

    mejoras

--- SportinHOUSE Version 1/js/validacion.js
+++ SportinHOUSE Version 1/js/validacion.js
@@ -1,13 +1,22 @@
-function validacions(f) {
+﻿function validacions() {
     
-    if (document.ingreso.usuario.length ==0) 
+    if (document.getElementById('modlgn_username').value =='') 
     { 
         alert('El nombre esta vacío'); 
-        document.ingreso.usuario.focus()
         return false; 
     }
     else{
-        window.open('http://127.0.0.1:51457/WebPage/Inventario/IndexInventario.html','','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, width=420, height=480, left=0, top=0');
+        if (document.getElementById('modlgn_username').value =='aede'){ 
+        <!-- document.location('WebPage/Inventario/IndexInventario.html','','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, width=1024, height=480, left=0, target="_blank", top=0');->
+        
+        }
+        else{
+            if (document.getElementById('modlgn_username').value =='amorales'){ 
+       document.location.
+	   <!-- document.location('WebPage/Seguridad/index.html','','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, width=1024, height=640, left=0, target="_blank", top=0');->
+        }
+            
+        }
     return true; 
     }
 }
